6cd33e28a4cc4e255708893bf75551abd4bbaa0c
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-06-30 12:41:15 -0300

    python

--- ex001.py
+++ ex001.py
@@ -1,2 +1,37 @@
-nome = input('Qual o seu nome? ')
-print('Prazer em te conhecer', nome)
+#nome = input('Qual o seu nome? ')
+#print('Prazer em te conhecer', nome)
+
+#nome2 = input('Digite sua idade:')
+#print('Você possui {} anos!'.format(nome2))
+
+
+'''
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número: '))
+s = n1 + n2
+print('A soma entre {} e {} vale {}' .format(n1, n2, s))
+
+n3 = input('Digite outro número: ')
+print(type(n3))
+print(type(int(-23)))
+print(type(True))
+print(type(3.56))
+
+# n = bool(input('Digte um número: '))
+#print(n)
+# Se eu respoder com algo vai dar True, se nada vai ser False
+'''
+n = input('Digte algo: ')
+print('é um número:' ,n.isnumeric())
+print('é uma letra:',n.isalpha())
+print('é um letraNúmero:',n.isalnum())
+print('é um espaço:', n.isspace())
+print('é um número deciaml:', n.isdecimal())
+# checar dpois
+
+'''
+int = número inteiros => 7, 0, -4
+float = números reais ou números de pontos flutuantes => 0.3, -12.432, 9.43, 7.0
+bool = valores booleanos, ou valores lógicos => True, False (a primeira letra maiuscula)
+str = valores caracteres ou strings => 'Olá', '7.5', '34', '', 'Olá mundo!'
+'''
