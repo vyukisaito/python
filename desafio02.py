e779d548e18543b6fa4f39dd1a519ed4126c7c01
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-09-17 22:29:08 -0300

    update

---
+++ desafio02.py
@@ -0,0 +1,11 @@
+import math
+#DESAFIO 016
+print('Desafio 016')
+num = float(input('Digite um número: '))
+nume = math.floor(num)
+print('O número {} tem a parte Inteira {}\n' .format(num, nume))
+
+#DESAFIO 017
+print('Desafio 017')
+co = float(input('Digite um cateto opost: '))
+ca = float(input('Agora digite um cateto adjacente: '))
