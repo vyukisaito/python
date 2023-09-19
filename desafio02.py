4df7e35da184adbd2ac18238d752fc482d25eb48
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-09-18 23:15:58 -0300

    update

--- desafio02.py
+++ desafio02.py
@@ -7,5 +7,9 @@ print('O número {} tem a parte Inteira {}\n' .format(num, nume))
 
 #DESAFIO 017
 print('Desafio 017')
-co = float(input('Digite um cateto opost: '))
+co = float(input('Digite um cateto oposto: '))
 ca = float(input('Agora digite um cateto adjacente: '))
+#hi = (co**2 + ca**2)**(1/2)
+#essa é uma das maneiras
+hi = math.hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}\n' .format(hi))
