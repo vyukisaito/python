f48e0f4bcd67420a5c3947960dac721d2c198951
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-09-20 22:58:26 -0300

    update

--- desafio02.py
+++ desafio02.py
@@ -13,3 +13,15 @@ ca = float(input('Agora digite um cateto adjacente: '))
 #essa é uma das maneiras
 hi = math.hypot(co, ca)
 print('A hipotenusa vai medir {:.2f}\n' .format(hi))
+
+#DESAFIO 018
+print('Desafio 018')
+angulo = float(input('Digite um ângulo qualquer: '))
+sen = math.sin(math.radians(angulo))
+print('O ângulo de {} tem o SENO de {}' .format(angulo, sen))
+cos = math.cos(math.radians(angulo))
+print('O ângulo de {} tem o COSSENO de {}' .format(angulo, cos))
+tan = math.tan(math.radians(angulo))
+print('O ângulo de {} tem o TANGENTE de {}' .format(angulo, tan))
+
+
