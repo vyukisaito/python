21b6badb1617b2471684c041ae476920c492fb37
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-09-25 22:14:31 -0300

    update

---
+++ powerup/app.py
@@ -0,0 +1,10 @@
+import pyautogui
+
+pyautogui.PAUSE = 1
+
+pyautogui.press('win')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+pyautogui.click(x=324, y=770)
+pyautogui.write('https://vyukisaito.github.io/enigma/68795-000/')
+pyautogui.press('enter')
