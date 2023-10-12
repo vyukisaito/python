7459d3edc8210431887a43110621dfd5a8b44e9d
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-10-11 23:44:00 -0300

    update

--- ex003.py
+++ ex003.py
@@ -1,3 +1,4 @@
+'''
 import math
 num = int(input('Digite um número: '))
 raiz = math.sqrt(num)
@@ -5,5 +6,14 @@ print(raiz)
 
 import random
 print(random.randint(1, 6))
+'''
 
-import emoji
+
+frase = 'São Paulo Maior Clube'
+print(frase[3]) # vai printar a quarta letra
+print(frase[3:13]) # vai da quarta letra até a 13
+print(frase[::2]) # vai saltar de duas em duas letras
+print(frase.count('o')) # vai contar quanto 'o' tem
+print(len(frase)) # vai contar quantos caracter tem contando com espaço
+frase = frase.replace('Clube', 'Seleção')
+print(frase)
