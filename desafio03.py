0a4ecc740c24dee39d2fbf6fea6ee613e68d5b28
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-10-17 23:19:14 -0300

    update

--- desafio03.py
+++ desafio03.py
@@ -8,4 +8,11 @@ print('Desafio 026')
 frase = str(input('Digite alguma coisa: ')).lower().strip()
 print('A letra A aparece {} vezes na frase' .format(frase.count('a')))
 print('A primeira letra A apareceu na posição {}' .format(frase.find('a')+1))
-print('A ultima letra A apareceu na posição {}' .format(frase.rfind('a')+1))
+print('A ultima letra A apareceu na posição {}\n' .format(frase.rfind('a')+1))
+
+#DESAFIO 026
+print('Desafio 026')
+n = str(input('Digite seu nome completo: ')).strip()
+nome = n.split()
+print('Seu primeiro nome é {}' .format(nome[0]))
+print('Seu último nome é {}' .format(nome[len(nome) -1 ]))
