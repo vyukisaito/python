c490f8a8bf81724420959d9ea29a2099b81d3caa
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-10-28 17:05:09 -0300

    update

--- desafio03.py
+++ desafio03.py
@@ -55,8 +55,11 @@ else:
 
 #DESAFIO 032
 print('Desafio 032')
-ano = int(input('Digite um ano: '))
+from datetime import date
+ano = int(input('Digite um ano (ou coloque 0 para analisar o seu): '))
+if ano == 0:
+    ano = date.today().year
 if (ano%4 == 0 and ano%100!=0) or (ano%400==0):
-    print('O ano é bissexto\n')
+    print('O ano {} é bissexto\n' .format(ano))
 else: 
-    print('O ano não é bissexto\n')
+    print('O ano {} não é bissexto\n' .format(ano))
--- desafio04.py
+++ desafio04.py
@@ -3,17 +3,17 @@ print('Desafio 033')
 um = int(input("Digite um número: "))
 dois = int(input("Digite outro um número: "))
 tres = int(input("Digite outro um número: "))
-if um > dois and um > tres:
-    print("O {} é o ma ior número \n" .format(um))
-elif um < dois and dois > tres:
-    print("O {} é o maior número \n" .format(dois))
-elif um < tres and dois < tres:
-    print("O {} é o maior número \n" .format(tres))
-elif um < dois and um < tres:
+if um>dois and um>tres:
+    print("O {} é o maior número" .format(um))
+if um<dois and dois> tres:
+    print("O {} é o maior número" .format(dois))
+if um<tres and dois<tres:
+    print("O {} é o maior número" .format(tres))
+if um<dois and um<tres:
     print("O {} é o menor número \n" .format(um))
-elif um > dois and dois < tres:
-    print("O {} é omenor número \n" .format(dois))
-elif um > tres and dois < tres:
+if um>dois and dois<tres:
+    print("O {} é o menor número \n" .format(dois))
+if um>tres and dois>tres:
     print("O {} é o menor número \n" .format(tres))
 # Não consegui fazer :(
 
@@ -24,3 +24,16 @@ if salario > 1250:
     print("Você receberá um aumento de 10%, com isso o seu salário é de {} \n" .format(salario + (salario * 0.1)))
 else:
     print("Você receberá um aumento de 15%, com isso o seu salário é de {} \n" .format(salario + (salario * 0.15)))
+    
+#DESAFIO 035
+print('Desafio 035')
+print("=-=" *20)
+print("Analisador de Triângulos")
+print("=-=" *20)
+s1 = float(input("Primeiro Segmento: "))
+s2 = float(input("Segundo Segmento: "))
+s3 = float(input("Terceiro Segmento: "))
+if s1 < s2 + s3 and s2 < s1 + s3 and s3 < s1 + s2:
+    print("Esses segmentos podem formar um triangulo")
+else:
+    print("Esses segmento não podem formar um triangulo")
