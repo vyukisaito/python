99453ab582fce3fb60fb77f548f49b7483f16f28
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-11-02 17:22:32 -0300

    update

--- desafio05.py
+++ desafio05.py
@@ -13,3 +13,20 @@ if primeiro_lado < segundo_lado + terceiro_lado and segundo_lado < primeiro_lado
         print("O seu triangulo é escaleno")
 else:
     print("Não pode formar um triangulo")
+
+#DESAFIO 043
+print('Desafio 043')
+peso = float(input("Qual o seu peso? (KG) "))
+altura = float(input("Qual a sua altura? (m) "))
+imc = peso / (altura * altura)
+print("O IMC dessa pessoa é de {:.1f}".format(imc))
+if imc < 18.5:
+    print("Abaixo do peso")
+elif 18.5 <= imc < 25:
+    print("Peso ideal")
+elif 25 <= imc < 30:
+    print("Sobrepeso")
+elif 30 <= imc < 40:
+    print("Obesidade ")
+elif imc >= 40:
+    print("Obersidade Mórbida")
--- teste.py
+++ teste.py
@@ -1,5 +1,17 @@
-#DESAFIO 043
-print('Desafio 043')
-peso = float(input("Qual o seu peso? (KG) "))
-altura = float(input("Qual a sua altura? (m)"))
-imc = peso / (altura * altura)
+# DESAFIO 044
+print('Desafio 044')
+print("="*30)
+print("          MERCADO")
+print("="*30)
+preco = float(input("Preço de compras: "))
+
+print('''FORMAS DE PAGAMENTO:
+[1] à vista dinheiro/cheque
+[2] à vista cartão
+[3] 2x no cartão
+[4] 3x ou mais no cartão
+''')
+opcao = int(input("Qual a opção? "))
+vista_dinheiro = preco * 0.9
+vista_cartao = preco * 0.5
+duas_vezes = preco / 2
