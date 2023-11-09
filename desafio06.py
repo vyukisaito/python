efce47f5f9dc63479467c622b347102603dd70c4
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-11-08 23:38:48 -0300

    update

--- desafio06.py
+++ desafio06.py
@@ -62,7 +62,7 @@ if total == 2:
     print("Por isso é PRIMO")
 else:
     print("Por isso NÃO é primo")
-''' 
+
 # Desafio 53
 frase = str(input("Digite um frase: ")).strip().upper()
 palavras = frase.split()
@@ -77,3 +77,60 @@ if inverso == junto:
     print("A frase digitada é um palindromo")
 else:
     print("A frase digitada não é um palindromo")
+
+# Desafio 54
+from datetime import date
+atual = date.today().year
+totmaior = 0
+totmenor = 0
+for i in range(1, 8):
+    nasc = int(input("Em que ano a {} pessoa nasceu?: " .format(i)))
+    idade = atual - nasc
+    if idade >= 21:
+        totmaior += 1 
+    else:
+        totmenor += 1
+print("Ao todo tivemos {} pessoas maior de idade" .format(totmaior))
+print("Ao todo tivemos {} pessoas menor de idade" .format(totmenor))
+
+# Desafio 55
+maior_peso = 0
+menor_peso = 0
+for i in range(1, 6):
+    peso = float(input("Peso da {} pessoa: " .format(i)))
+    if peso == 1:
+        maior_peso = peso
+        menor_peso = peso
+    else:
+         if peso > maior_peso:
+             maior_peso = peso
+         if peso < menor_peso:
+             menor_peso = peso
+print("O maior peso lideo foi {}" .format(maior_peso))
+print("O menor peso lideo foi {}" .format(menor_peso))
+''' 
+
+# Desafio 56
+media = 0
+soma = 0
+maior_homem = 0
+nome_velho = ''
+totmulher20 = 0
+for i in range(1, 5):
+    print("----- {} PESSOA -----" .format(i))
+    nome = str(input("Nome: ")).strip()
+    idade = int(input("Idade: "))
+    sexo = str(input("Sexo [M/F]: ")).strip()
+    soma += idade
+    if i == 1 and sexo in "Mm":
+        maior_homem = idade
+        nome_velho = nome
+    if sexo in "Mn" and idade > maior_homem:
+        maior_homem = idade
+        nome_velho = nome
+    if sexo in "Ff" and idade < 20:
+        totmulher20 += 1
+media = soma / 4
+print("A media de idade do grupo é de {} anos" .format(media))
+print("O homem mais velho tem {} anos, e se chama {}" .format(maior_homem, nome_velho))
+print("Ao todo são {} mulheres com menos de 20 anos" .format(totmulher20))
