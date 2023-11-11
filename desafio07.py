02da0001575fd2c1575b842a2262d37098c02eef
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-11-10 23:39:01 -0300

    update

--- desafio07.py
+++ desafio07.py
@@ -4,7 +4,7 @@ while r == "S":
     n = int(input("Digite um valor: "))
     r = str(input("Quer continuar? [S/N] ")).upper()
 print("FIM")
-'''
+
 i = 1
 par = impar = 0
 while i != 0:
@@ -15,3 +15,9 @@ while i != 0:
         else:
             impar += 1
 print("Você digitou {} número pares e {} números impares!" .format(par, impar))
+'''
+# Desafio 57
+sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
+while sexo not in 'MF':
+    sexo = str(input("Dados inválidos. Por favor insira o seu sexo: ")).strip().upper()[0]
+print("Sexo {} registrado com sucesso".format(sexo))
