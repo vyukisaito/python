f9fd0e4b7289d6fe057378969b7a692058b9ee05
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-11-26 23:09:23 -0300

    update

--- listas-parte1.py
+++ listas-parte1.py
@@ -63,12 +63,75 @@ while True:
         break
 numeros.sort()
 print(f"Você digitou os valores: {numeros}")
-'''
 
 # Desafio 80
-num = list()
-n1 = 0
+# num.insert(3, 9)
+num = []
 for i in range(0, 5):
-    n = int(input("Digite um valor: "))
-    
+    n = int(input("Digite um número: "))
+    if i == 0 or n > num[-1]:
+        print("O valor foi inserido na última posição...")
+        num.append(n)
+    else:
+        pos = 0
+        while pos < len(num):
+            if n <= num[pos]:
+                num.insert(pos, n)
+                print(f"Adicionando o valor na posição {pos}")
+                break
+            pos += 1
+print(num)
+
+# Desafio 81
+lista = list()
+while True:
+    num = int(input("Digite um número: "))
+    lista.append(num)
+    opcao = str(input("Quer continuar [S/N]? ")).strip().upper()[0]
+    if opcao in 'N':
+        break
+lista.sort(reverse=True)
+print(f"Você digitou {len(lista)} elementos")
+print(f"Os valores em ordem decrescente são {lista}")
+if 5 in lista:
+    print("O valor 5 faz parte da lista")
+else: 
+    print("O valor 5 não faz parte da lista")
+
+# Desafio 82
+lista = list()
+while True:
+    num = int(input("Digite um número: "))
+    lista.append(num)
+    opcao = str(input("Quer coninuar? ")).strip().upper()[0]
+    if opcao in 'N':
+        break
+print(f"A lista completa é {lista}")
+pares = []
+impares = []
+for i in lista:
+    if i % 2 == 0:
+        pares.append(i)
+    else:
+        impares.append(i)
+print(f"A lista de pares é {pares}")
+print(f"A lista de impares é {impares}")
+'''
+
+# Desafio 83
+expr = str(input("Digite um expressão: "))
+lista = []
+for i in expr:
+    if i == '(':
+        lista.append('(')
+    elif i == ')':
+        if len(lista) > 0:
+            lista.pop()
+        else:
+            lista.append(')')
+            break
+if len(lista) == 0:
+    print("Expressão válida")
+else:
+    print("Expressão errada")
 
