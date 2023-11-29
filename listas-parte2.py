e2de72e57dac3db1bff1f32a9d5ef2a5468bf22a
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-11-28 23:05:54 -0300

    update

--- listas-parte2.py
+++ listas-parte2.py
@@ -27,8 +27,6 @@ for i in pessoas:
         print(f'{i[0]} é maior de idade')
     else:
         print(f'{i[0]} é menor de idade')
-'''
-
 
 # Desagio 84
 dados = list()
@@ -60,3 +58,74 @@ print(f"O menor peso foi de {menor}KG. Peso de ", end='')
 for i in pessoas:
     if i[1] == menor:
         print(f"{i[0]} ", end='')
+
+# Desafio 85
+num = [[], []]
+valor = 0
+for i in range(1, 8):
+    valor = int(input(f"Digite o {i}o valor: "))
+    if valor % 2 == 0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+print(f"Os valores pares digitados foram: {num[0]}")
+print(f"Os valores impares digitados foram: {num[1]}")
+
+# Desafio 86 e 87
+num = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+par = soma = teceira = maior = 0
+for l in range(0, 3):
+    for c in range(0, 3):
+        num[l][c] = int(input(f"Digite um número na posição [{l}, {c}]: "))
+
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f"[{num[l][c]:^5}]", end='')
+        if num[l][c] % 2 == 0:
+            par += 1
+            if par == 1:
+                soma = num[l][c]
+            else:
+                soma += num[l][c]
+        
+        for i in range(0, 3):
+            if i == 0:
+                maior = num[1][i]
+                terceira = num[i][2]
+            else:
+                terceira += num[i][2]
+                if maior < num[1][i]:
+                    maior = num[1][i]
+
+    print()
+print(f"A soma dos valores pares é {soma}")
+print(f"A soma dos valores da terceira coluna é {terceira}")
+print(f"O maior valor da segunda linha é {maior}")
+
+# Desafio 88
+from random import randint
+from time import sleep
+print('SORTEADOR'.center(30))
+lista = [[], [], [], [], [], []]
+jogos = int(input("Quantos jogos você quer que eu sorteie? "))
+for i in range(1, jogos+1):
+    for c in range(0,6):
+        if c == 0:
+            lista[c] = randint(1, 60)
+        else:
+            while True:
+                if lista[c] == lista[c-1]:
+                    lista[c] = randint(1, 60)
+                else:
+                    lista[c] = randint(1, 60)
+                    break
+    lista.sort()
+    print(f"Jogo {i}:  {lista}")
+    lista = [[], [], [], [], [], []]
+    sleep(1)
+'''
+
+aluno = [ ['caio',[12]], ['Maria',[12]] ]
+teste = ['caio',[23]]
+aluno.append(teste)
+print(aluno)
