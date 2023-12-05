0be50ae3f0e9c2173d18bd607e2ec7ad7de15001
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-12-05 00:45:40 -0300

    update

---
+++ funcoes-parte1.py
@@ -0,0 +1,71 @@
+'''
+def msg(teste):
+    print('------------')
+    print(teste)
+    print('------------')
+
+
+msg('Teste')
+def soma(a, b):
+    print(f"A soma entre {a} e {b} é : {a+b}")
+soma(b=5, a=6)
+
+print()
+def contador(*num):
+    for i in num:
+        print(i, end=' ')
+    print()
+
+
+contador(3, 4, 5)
+contador(3, 1, 5, 3)
+
+def dobra(lista):
+    pos = 0
+    while pos < len(lista):
+        lista[pos] *= 2
+        pos += 1    
+valores = [1, 2, 3, 4, 5, 6]
+dobra(valores)
+print(valores)
+
+# Desafio 96
+def area(a, b):
+    print(f'A area de um terreno {a}x{b} é de {a*b}m²')
+
+largura = float(input('LARGURA (m): '))
+comprimento = float(input('COMPRIMENTO (m): '))
+area(largura, comprimento)
+
+# Desafio 97
+def escreva(msg):
+    tamanho = len(msg) + 4
+    print('-'*tamanho)
+    print(f'  {msg}')
+    print('-'*tamanho)
+escreva('TEste')
+'''
+
+# Desafio 98
+print('Contagem de 1 até 10 em 1 e 1')
+for i in range(1, 11):
+    print(f'{i}', end=' ')
+print("FIM!")   
+
+print('Contagem de 10 até 0 em 2 e 2')
+for i in range(10, -1, -2):
+    print(i, end=' ')
+print("FIM!")
+
+def calc(a, b, c):
+    print(f'Contagem de {a} até {b} de {c} em {c}')
+    if a < b:
+        for i in range(a, b+1, c):
+            print(i, end=" ")
+    else:
+        for i in range(a, b - 1, -c):
+            print(i, end=" ")
+inicio = int(input("Início: "))
+fim = int(input("Fim: "))
+passo = int(input("Passo: "))
+calc(inicio, fim, passo)
